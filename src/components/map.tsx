423c0e500b596ab1c2d8c0c636894b5fbfd8868b
Ando Deng <87212291+AD1938@users.noreply.github.com>
2024-07-12 14:50:50 -0400

    add branches

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -14,17 +14,19 @@ type MarkerWithAnimation = Location & { animation: google.maps.Animation | undef
 
 const initialLocations: MarkerWithAnimation[] = [
   { lat: 43.8581514, lng: -79.3402475, name: 'Head Office', address: 'Unit 101, 200 Town Centre Boulevard, Markham, Ontario, Canada, L3R 8H8', phone: '+905-234-6666', email: 'info@wellcareinsurance.ca', animation: undefined },
+  { lat: 42.9762946, lng: -81.2406115, name: 'London Office', address: '302-120 Wellington St, London, ON, N6B 2K6', phone: '+905-234-6666', email: 'info@wellcareinsurance.ca', animation: undefined },
   { lat: 43.8537388, lng: -79.369347, name: 'Markham West', address: 'Unit 703, 90 Allstate Parkway, Markham, ON L3R 6H3', phone: '+289-301-5887', animation: undefined },
   { lat: 43.7728107, lng: -79.3310703, name: 'North York Consumers Office', address: 'Unit 502, 200 Consumers Rd., North York, ON M2J 4R4', phone: '+289-301-5865', animation: undefined },
   { lat: 43.8056598, lng: -79.3405279, name: 'North York Tempo Office', address: ' Suite 310, 100 TEMPO AVE, NORTH YORK,  ON, M2H 2N8', phone: '+647-643-1992', animation: undefined },
+  { lat: 45.3695035, lng: -75.7044042, name: 'Ottawa Office', address: '101K-900 Dynes Road, Ottawa, ON', phone: '+905-234-6666', animation: undefined }, 
   { lat: 43.850908, lng: -79.3871378, name: 'Richmond Hill Office', address: ' Suite 212, 9040 Leslie St, Richmond Hill,  ON, L4B 3M4', phone: '+647-643-1992', animation: undefined },
   { lat: 43.8054131, lng: -79.5277074, name: 'Vaughan Office', address: 'Unit 202, 11 Cidermill Ave, Concord, ON, L4K 4B6', phone: '+905-760-5007', animation: undefined },
 
 ];
 
 const containerStyle = {
-  width: '650px',
-  height: '800px'
+  width: '700px',
+  height: '1100px'
 };
 
 const MyGoogleMapComponent: React.FC = () => {
